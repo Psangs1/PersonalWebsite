02a97125a19dfecfca78ba9b6824d4577013c10b
Psangs123 <39105705+FearlessTiger@users.noreply.github.com>
2025-04-21 13:42:08 -0400

    navbar fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,14 @@ import { HashLink } from 'react-router-hash-link';
 const Navbar = () => {
     return (
         <>
-        <nav className='pb-20'>
+        <nav className='hidden md:block pb-20'>
             <ul>
                 <div className='flex justify-between font-primary sm:text-balance'>
-                    <div className='flex flex-col items-start text-left'> 
+                    <div className='flex flex-col md:items-start md:text-left text-center items-center'> 
                         <p> Parth Sangani</p>
                         <p> University of Maryland, College Park</p>
                     </div>
-                    <div className='font-bold flex flex-row gap-15'>
+                    <div className='font-bold flex flex-row gap-15' id='navbar-default'>
                         <HashLink smooth to="/#hero">
                             <li>About</li>
                         </HashLink>
