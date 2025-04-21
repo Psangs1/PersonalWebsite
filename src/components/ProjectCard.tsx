ed3c4f946f4f45ecfafd34e8bbd72f829be2c9a3
Psangs123 <39105705+FearlessTiger@users.noreply.github.com>
2025-04-21 11:21:30 -0400

    additional changes to project showcase

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -14,7 +14,7 @@ const ProjectCard: React.FC<Project> = ({name, skills, dates, image, description
           </div>
         </div>
 
-        <div className="h-[250px] w-full max-w-[500px] pt-10 mx-auto overflow-hidden flex justify-center items-center rounded-2xl bg-gray-200">
+        <div className="sm:max-h-[500px] h-[250px] w-full max-w-[500px] pt-10 mx-auto flex justify-center items-center rounded-2xl bg-gray-200">
           <img 
             src={image} 
             className="h-full object-cover rounded-2xl"
@@ -26,7 +26,7 @@ const ProjectCard: React.FC<Project> = ({name, skills, dates, image, description
           <h1>{description}</h1>
         </div>
        
-       <div className="flex flex-row-reverse items-end justify-items-end gap-8 font-primary">
+       <div className="flex md:flex-row-reverse flex-row items-end justify-items-end gap-8 font-primary">
         <a href={websitelink} target="_blank">
           <button className="w-[150px] h-[50px] text-white bg-black text-sm font-semibold rounded-3xl drop-shadow-xl hover:bg-gray-800 transition delay-50 duration-300 ease-in-out ">
             Visit Website
