bd4bbec999192c0b0fabe16848d41cee307cb661
Psangs123 <39105705+FearlessTiger@users.noreply.github.com>
2025-04-23 10:47:28 -0400

    footer final changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@
 const Footer = () => {
     return (    
         <>
-        <footer className="rounded-lg shadow-2xl mt-30 p-4">
+        <footer className="rounded-lg mt-30 p-4">
             <div className="md:flex md:items-center md:justify-between align-bottom">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" className="hover:underline">Parth Sangani™</a>
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://parth-sangani.vercel.app" className="hover:underline">Parth Sangani™</a>
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
